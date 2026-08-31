7170a2081a61e4edfb64c7f3e0b26a3d02621ac5
c0derArmy <mahavirkantsingh8524@gmail.com>
2026-08-31 16:26:47 +0530

    Remove unnecessary files

--- session.py
+++
@@ -1,8 +0,0 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-
-api_id = 37998813
-api_hash = "49b0f0677294b1918b255d3b16a21027"
-
-with TelegramClient(StringSession(), api_id, api_hash) as client:
-    print(client.session.save())
